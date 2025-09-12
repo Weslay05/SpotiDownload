efc32f09fe31e6d95de901ba2907bf9c487747d2
Weslay <weslaytchibindi3@gmail.com>
2025-09-12 04:04:23 +0200

    Update main_for_tracks.bat

--- main_for_tracks.bat
+++ main_for_tracks.bat
@@ -1,5 +1,7 @@
 call activate ./conda-windows
 
+title Spotify Downloader Batch
+
 REM Loop through each line in tracks.txt
 for /f "usebackq delims=" %%a in ("assets/tracks.txt") do (
     echo Processing %%a
