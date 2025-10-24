c77cbd7581541bad781c67b36e8903bffd5c5e0b
Weslay05 <weslaytchibindi3@gmail.com>
2025-10-24 14:59:36 +0200

    Move and Renamed bat file

--- bin/windows/tracks-txt.bat
+++ bin/windows/tracks-txt.bat
@@ -1,11 +1,12 @@
-call activate ./conda-windows
+cd ../../
+call activate ./conda-debian
 
 title Spotify Downloader Batch
 
 REM Loop through each line in tracks.txt
 for /f "usebackq delims=" %%a in ("assets/tracks.txt") do (
     echo Processing %%a
-    python main.py --spotify_url "%%a"
+    python main.py --spotify "%%a"
 )
 
 echo done
